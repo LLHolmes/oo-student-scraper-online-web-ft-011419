369aee6b601d291f04ed91dd603e6d36a938448a
Lana Holmes <lana.lee.holmes@gmail.com>
2019-02-01 18:43:47 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -35,4 +35,4 @@ class Scraper
 end
 
 # Scraper.scrape_index_page("./fixtures/student-site/index.html")
-Scraper.scrape_profile_page()
+Scraper.scrape_profile_page("./fixtures/student-site/students/adrian-prieto.html")
