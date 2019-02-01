e54a804e4cf9241d1be373723e052863c6137a69
Lana Holmes <lana.lee.holmes@gmail.com>
2019-02-01 16:16:37 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,19 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    all_students = []
+    doc = Nokogiri::HTML(open(index_url))
+    student_elements = doc.css('.student-card')
+    student_elements.each do |student|
+      student[:name] = student.css('.student-name').text
+      student[:location] = student.css('.student-location').text
+      student[:profile_url] = ""
+    binding.pry
+    #[{:name => "" :location => "", :profile_url => ""}, {}, {}]
   end
 
   def self.scrape_profile_page(profile_url)
@@ -13,3 +22,4 @@ class Scraper
 
 end
 
+Scraper.scrape_index_page("./fixtures/student-site/index.html")
