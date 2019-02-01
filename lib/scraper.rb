cb4e09a56376222e8438c49b9abe27fa88030e93
Lana Holmes <lana.lee.holmes@gmail.com>
2019-02-01 19:56:57 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -27,10 +27,8 @@ class Scraper
     social_media = doc.css('.social-icon-container').css('a')
     social_media.each do |icon|
       if icon.attr('href').include?("twitter.com")
+        profile_hash[:twitter] = icon.attr('href')
         
-        github.com || www.github
-        twitter.co || www.twitte
-        linkedin.c || www.linked
     doc.css('.social-icon-container').css('a')[0].attr('href')
     # twitter = "https://twitter.com"
     # linkedin = "https://www.linkedin.com" // "https://www.linkedin.com"
